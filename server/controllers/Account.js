8131fe38a6febe13cf33d2be7c5aedcfa5876aac
Matthew Ratzell <mjr4427@g.rit.edu>
2022-03-26 15:48:05 -0400

    finished domomaker b

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -6,8 +6,11 @@ const loginPage = (req, res) => res.render('login');
 
 const signupPage = (req, res) => res.render('signup');
 
-const logout = (req, res) => res.redirect('/');
-
+const logout = (req, res) => {
+  //make sure we destroy mcookies on the schmove
+  req.session.destroy();
+  res.redirect('/');
+}
 const login = (req, res) => {
   const username = `${req.body.username}`;
   const pass = `${req.body.pass}`;
@@ -20,6 +23,7 @@ const login = (req, res) => {
     if (err || !account) {
       return res.status(400).json({ error: 'All fields are required!' });
     }
+    req.session.account = Account.toAPI(account);
     return res.json({ redirect: '/maker' });
   });
 };
@@ -41,6 +45,7 @@ const signup = async (req, res) => {
     const hash = await Account.generateHash(pass);
     const newAccount = new Account({ username, password: hash });
     await newAccount.save();
+    req.session.account = Account.toAPI(newAccount);
     return res.json({ redirect: '/maker' });
   } catch (err) {
     console.log(err);
